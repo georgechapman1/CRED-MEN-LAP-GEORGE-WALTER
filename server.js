35630c88bba1dee32574f97f6f6d0698870a15e0
George Chapman <George@Georges-MacBook-Pro.local>
2019-10-18 14:34:18 -0600

    create form and post to index

--- server.js
+++ server.js
@@ -9,10 +9,44 @@ app.use(bodyParser.urlencoded({extended: false}))
 //tie data to server
 const teams = require('./models/teams.js')
 
+
+//make a new team page
 app.get('/teams/new', (req, res) => {
     res.render('new.ejs')
 })
 
+
+
+app.post('/teams', (req, res) => {
+    // our routes are the final destination
+    console.log(req.body, ' this should be the contents of the form')
+
+    // create the teams/ use the model to add it to our db
+    // MOdel to add it to the array (in this case we have an array)
+    // representing our db, teams is our array
+    teams.push(req.body);
+    console.log(teams)
+    // I'm getting /teams from the url in the index route
+    res.redirect('/teams');
+})
+
+
+
+// display index route
+app.get('/teams', (req, res) => {
+      res.render('index.ejs', {
+        teams: teams, //IndexRouteteams
+        index: req.params.id
+    });
+});
+
+// // display show route
+// app.get('/teams/:id', (req, res) => {
+//       res.render('show.ejs', {
+//         SRteams: teams[req.params.id], //ShowRouteteam
+//         index: req.params.id
+//     });
+// });
 
 
 
