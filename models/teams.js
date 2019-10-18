b07d35c93415af19eda750b5082482e997c3cbf5
George Chapman <George@Georges-MacBook-Pro.local>
2019-10-18 14:54:20 -0600

    answers to quests are in teams.js

--- models/teams.js
+++ models/teams.js
@@ -13,3 +13,13 @@ const teamsSchema = new mongoose.Schema ({
 const Teams = mongoose.model('Teams', teamsSchema);
 
 module.exports = Teams;
+
+
+// 1)Schema: Schema is a blueprint 
+//for what our documents (the objects we put into mongodb) should look like, we define the keys,
+//and the types of the values
+// 2)Model: The model is the object that stores our documents/collections
+// 3) Relationship is the model can store the documents that are created through requirements of schema
+// 4) Schema gives parameters of what is required for data to be created into a document and then the model stores, 
+// or acts like a container, for many documents
+
