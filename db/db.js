74745cde6f849891e758507cd9e3bb677bce2ec1
George Chapman <George@Georges-MacBook-Pro.local>
2019-10-18 14:43:43 -0600

    connected to mongod

---
+++ db/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+//naming database, named teams
+const connectionString = 'mongodb://localhost/teams'; 
+
+mongoose.connect(connectionString, { useNewUrlParser: true,
+                                     useUnifiedTopology: true,
+                                     useCreateIndex: true,
+                                     useFindAndModify: false
+                                    });
+
+
+
+// to check if database is connected, disconnected, or error
+// need to connect to server.js
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${connectionString}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose is disconnected`);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err, 'mongoose error');
+});
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // const methodOverride = require('method-override')
 const bodyParser = require('body-parser');
-
+require('./db/db');
 app.use(bodyParser.urlencoded({extended: false}))
 // app.use(methodOverride('_method'))
 
